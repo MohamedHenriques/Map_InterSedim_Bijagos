3f102954cc3238a3ca56298d3791b9857ed4026b
Mohamed Henriques <mhenriquesbalde@gmail.com>
2020-01-16 16:45:34 +0000

    Progress 202001161645

--- Scripts/Intertidal_class1.R
+++ Scripts/Intertidal_class1.R
@@ -44,7 +44,48 @@ names(S2_resamp)<-c("B05_10","B06_10","B07_10","B08a_10","B09_10","B11_10","B12_
 ## Merge all bands together
 S2_20190316<-stack(S2_10,S2_resamp)
 S2_20190316
-nlayers(S2_20190316)
 
 
-batim<-raster()
+## load batimetry raster from catalao 
+batim<-raster("D:/Work/FCUL/Doutoramento/Capitulos/Mapping_intertidal_sediments/Jcatalao/bijagos_batim.tif")
+batim
+ggR(batim)
+
+## check the extents of the two layers -- if they are different crop both datasets
+if (extent(S2_20190316) == extent(batim)){
+  print("Extents are the same, no need to crop")
+} else {
+  # calculate overlap between the two datasets
+  overlap <- intersect(extent(S2_20190316), extent(batim))
+  # now let's crop both datasets to the overlap region
+  S2_20190316_c <- crop(S2_20190316, overlap)
+  batim_c <- crop(batim, overlap)
+  if (extent(S2_20190316_c) == extent(batim_c)){
+    print("Extents are different, data cropped and solved")
+  } else {
+    # resample batim using sat image
+    batim_r<-resample(batim_c,S2_20190316_c, method="bilinear")
+    if (extent(S2_20190316_c)==extent(batim_r)){
+      print("Cropping failed. Batim resampled, solved. Use batim_r to mask S2_20190316_c")
+    } else {
+      print("Failed, try another thing")
+    }
+  }
+  
+}
+
+S2_20190316_crop<-crop(S2_20190316,batim)
+plotRGB(S2_20190316_crop,3,2,1,stretch="lin")
+extent(S2_20190316_crop)==extent(batim))
+
+
+
+batim_r
+ggR(batim_r)
+ggRGB(S2_20190316,3,2,1,stretch="lin")
+plotRGB(S2_20190316_c,3,2,1,stretch="lin")
+
+S2_20190316_cm<-mask(S2_20190316_c,batim_r)
+
+Adon_b<-readOGR("./Shapefiles/Adonga_bancos/poligono1.shp")
+  
