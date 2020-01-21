119e35bf658c41de9acdd7a4e722d6801f630672
Mohamed Henriques <mhenriquesbalde@gmail.com>
2020-01-21 21:53:42 +0000

    progressos 202001212153

--- Scripts/Ground_truthing_2020.R
+++ Scripts/Ground_truthing_2020.R
@@ -2,3 +2,16 @@ setwd("D:/Work/FCUL/Doutoramento/R/Mapping_coastal_Habitats_Guinea_Bissau/Github
 rm(list=ls())
 graphics.off()
 
+packs<-c("RStoolbox","raster","ggplot2","rgdal")
+lapply(packs,require,character.only=T)
+
+
+
+## Load S2 image 
+S2_20200105_a<-stack("D:/Work/FCUL/Doutoramento/Capitulos/Mapping_intertidal_sediments/SNAP/S2/Resampled/subset_S2B_MSIL2A_20200105T112349_N0213_R037_T28PCT_20200105T123330_resampled.tif")
+
+##Check image
+names(S2_20200105_a)<-c("B02","B03","B04","B05","B06","B07","B08","B08a","B09","B11","B12")
+names(S2_20200105_a)
+
+ggRGB(S2_20200105_a,3,2,1,stretch = "lin")
--- Scripts/Intertidal_class1.R
+++ Scripts/Intertidal_class1.R
@@ -12,8 +12,6 @@ library(maptools)
 library(cluster)
 library(rgdal)
 library(sentinel2)
-<<<<<<< HEAD
-<<<<<<< HEAD
 library(rasterVis)
 library(RColorBrewer)
 library(viridis)
@@ -286,11 +284,3 @@ plot(kmeans_raster_Adongabancos_1, col=magma(5), main="kmeans_radar_Adongabancos
 
 
 
-=======
-
-##Aditional line just to see
->>>>>>> b7c8e457a9dd3fbfc2197ea14047b0e471e4b4b6
-=======
-
-##Aditional line just to see
->>>>>>> b7c8e457a9dd3fbfc2197ea14047b0e471e4b4b6
