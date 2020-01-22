bc4d59d5fb3718f59cc795edd0b29a2c1a3e5521
Mohamed Henriques <mhenriquesbalde@gmail.com>
2020-01-22 14:36:06 +0000

    progressos 202001221436

--- Scripts/Ground_truthing_2020.R
+++ Scripts/Ground_truthing_2020.R
@@ -2,7 +2,7 @@ setwd("D:/Work/FCUL/Doutoramento/R/Mapping_coastal_Habitats_Guinea_Bissau/Github
 rm(list=ls())
 graphics.off()
 
-packs<-c("RStoolbox","raster","ggplot2","rgdal")
+packs<-c("RStoolbox","raster","ggplot2","rgdal","viridis")
 lapply(packs,require,character.only=T)
 
 
@@ -22,9 +22,6 @@ plotRGB(S2_20200105_a,3,2,1,stretch = "lin")
 plotRGB(S2_20200105_b,3,2,1,stretch = "lin")
 
 ##Load S1 SAR images
-
-
-
 
 ###Poligonos areas de trabalho
 gnb<-readOGR(dsn="Shapefiles/GNB",layer="gnb_poly")
@@ -88,16 +85,119 @@ plot(bat)
 
 # Mask out every study area
 
-## Mask Urok
+## Mask Urok with bathymetry
 mask_urok<-crop(bat,Urok)
 plotRGB(S2a_Urok,3,2,1,stretch = "lin")
-plot(mask_urok,add=T)
+plot(mask_urok)
 
+
+
+## check the extents of the two layers -- if they are different crop both datasets
+if (extent(S2a_Urok) == extent(mask_urok)){
+  print("Extents are the same, no need to crop")
+} else {
+  # calculate overlap between the two datasets
+  overlap <- intersect(extent(S2a_Urok), extent(mask_urok))
+  # now let's crop both datasets to the overlap region
+  S2a_Urok_c <- crop(S2a_Urok, overlap)
+  mask_urok_c <- crop(mask_urok, overlap)
+  if (extent(S2a_Urok_c) == extent(mask_urok_c)){
+    print("Extents are different, data cropped and solved")
+  } else {
+    # resample batim using sat image
+    mask_urok_r<-resample(mask_urok_c,S2a_Urok_c,method="bilinear")
+    if (extent(S2a_Urok_c)==extent(mask_urok_r)){
+      print("Cropping failed. Batim resampled, solved. Use mask_urok_r to mask S2a_Urok_c")
+    } else {
+      print("Failed, better try fishing")
+    }
+  }
+  
+}
+Intertidal_urok<-mask(S2a_Urok_c,mask_urok_r) ### This mask is no good, has too much water
+plotRGB(Intertidal_urok,3,2,1,stretch="lin")
+
+
+##
+sa_a<-c(S2a_Urok,S2a_Bub)
+sa_b<-c(S2b_CanhGa,S2b_Bolama)
+names_a<-c("Urok","Bub")
+names_b<-c("CanhGa","Bolama")
+
+## Mask with NDWI - this is better for now
+
+###Urok
 ndwi_Urok<-(S2a_Urok$B03-S2a_Urok$B08)/(S2a_Urok$B03+S2a_Urok$B08)
 plot(ndwi_Urok)
-hist(ndwi_Urok, n=1000, freq=T)
-axis(1)
+hist(ndwi_Urok, n=1000, freq=T,axes=F)
+axis(1,at=c(seq(-0.8,0.8,0.05)))
+axis(2)
 
-table(ndwi_Urok@data@values)
+ndwi_Urok_F<-cut(ndwi_Urok,breaks=c(-2,-.30,.20,2))
+plot(ndwi_Urok_F)
+table(ndwi_Urok_F@data@values)
 
+mask_urok1<-ndwi_Urok_F==2
+plot(mask_urok1)
+mask_urok1[mask_urok1==0]<-NA
+plot(mask_urok1,colNA=1)
 
+Intertidal_urok1<-S2a_Urok*mask_urok1
+plotRGB(Intertidal_urok1,3,2,1,stretch = "lin")
+
+###Bub
+ndwi_Bub<-(S2a_Bub$B03-S2a_Bub$B08)/(S2a_Bub$B03+S2a_Bub$B08)
+plot(ndwi_Bub)
+hist(ndwi_Bub, n=1000, freq=T,axes=F)
+axis(1,at=c(seq(-0.8,0.8,0.05)))
+axis(2)
+
+ndwi_Bub_F<-cut(ndwi_Bub,breaks=c(-2,-.425,.15,2))
+plot(ndwi_Bub_F)
+table(ndwi_Bub_F@data@values)
+
+mask_Bub1<-ndwi_Bub_F==2
+plot(mask_Bub1)
+mask_Bub1[mask_Bub1==0]<-NA
+plot(mask_Bub1,colNA=1)
+
+Intertidal_Bub1<-S2a_Bub*mask_Bub1
+plotRGB(Intertidal_Bub1,3,2,1,stretch = "lin")
+
+###CanhGa
+ndwi_CanhGa<-(S2b_CanhGa$B03-S2b_CanhGa$B08)/(S2b_CanhGa$B03+S2b_CanhGa$B08)
+plot(ndwi_CanhGa)
+hist(ndwi_CanhGa, n=1000, freq=T,axes=F)
+axis(1,at=c(seq(-0.8,0.8,0.05)))
+axis(2)
+
+ndwi_CanhGa_F<-cut(ndwi_CanhGa,breaks=c(-2,-.30,.20,2))
+plot(ndwi_CanhGa_F)
+table(ndwi_CanhGa_F@data@values)
+
+mask_CanhGa1<-ndwi_CanhGa_F==2
+plot(mask_CanhGa1)
+mask_CanhGa1[mask_CanhGa1==0]<-NA
+plot(mask_CanhGa1,colNA=1)
+
+Intertidal_CanhGa1<-S2b_CanhGa*mask_CanhGa1
+plotRGB(Intertidal_CanhGa1,3,2,1,stretch = "lin")
+
+###Bolama
+ndwi_Bolama<-(S2b_Bolama$B03-S2b_Bolama$B08)/(S2b_Bolama$B03+S2b_Bolama$B08)
+plot(ndwi_Bolama)
+hist(ndwi_Bolama, n=1000, freq=T,axes=F)
+axis(1,at=c(seq(-0.8,0.8,0.05)))
+axis(2)
+
+ndwi_Bolama_F<-cut(ndwi_Bolama,breaks=c(-2,-.35,.20,2))
+plot(ndwi_Bolama_F)
+table(ndwi_Bolama_F@data@values)
+
+mask_Bolama1<-ndwi_Bolama_F==2
+plot(mask_Bolama1)
+mask_Bolama1[mask_Bolama1==0]<-NA
+plot(mask_Bolama1,colNA=1)
+
+Intertidal_Bolama1<-S2b_Bolama*mask_Bolama1
+plotRGB(Intertidal_Bolama1,3,2,1,stretch = "lin")
