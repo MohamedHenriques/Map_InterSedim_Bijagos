4839cfbc1065569d1528b68061cfbae891e921e5
Mohamed Henriques <mhenriquesbalde@gmail.com>
2020-11-14 23:05:16 +0000

    dherhrerh

--- Scripts/Granulometry_test.R
+++ Scripts/Granulometry_test.R
@@ -7,24 +7,33 @@ packs<-c("raster","ggplot2","rgdal","viridis","sp","RColorBrewer","scales","tool
 lapply(packs,require,character.only=T)
 
 ##Load granulometry data
-Gra<-read.xlsx("D:/Work/FCUL/Doutoramento/Capitulos/Mapping_intertidal_sediments/Data/Granulometry/Data_gran_20201109.XLSX",1)
+Gra<-read.xlsx("D:/Work/FCUL/Doutoramento/Capitulos/Mapping_intertidal_sediments/Data/Granulometry/Data_gran_20201113_OK.XLSX",1)
 str(Gra)
 colnames(Gra)[3:7]<-c(2000,500,250,63,0)
 
-Gra1<-Gra[-which(Gra$Sed_ID==100),c(1,3:12)] # clean database 
-
+Gra1<-Gra[-which(Gra$Sed_ID==100 | Gra$Sed_ID==242),c(1,3:7)] # clean database 
+#Gra11<-Gra[-which(Gra$Sed_ID==242),]
 ##Transpose dataframe
 a<-melt(Gra1,id.vars = "Sed_ID")
 Gra2<-dcast(a,variable~Sed_ID,fun.aggregate = mean)
 Gra2$variable<-c(2000,500,250,63,0)
 rownames(Gra2)<-Gra2$variable
 Gra3<-Gra2[,-1]
+#colnames(Gra3)<-paste("X",colnames(Gra3),sep="_")
 
 ###Data with percent for ploting
-Gra4<-Gra2[c(6:10),]
-Gra5<-melt(Gra4,id.vars = "variable")
-colnames(Gra5)[1:2]<-c("crivo","Sed_ID")
-Gra6<-dcast(Gra5,Sed_ID~crivo,fun.aggregate = mean)
+Gra11<-Gra[-which(Gra$Sed_ID==100 | Gra$Sed_ID==242),c(1,8:12)] # clean database 
+
+aa<-melt(Gra11,id.vars = "Sed_ID")
+Gra22<-dcast(aa,variable~Sed_ID,fun.aggregate = mean)
+Gra22$variable<-c(2000,500,250,63,0)
+rownames(Gra22)<-Gra22$variable
+#Gra33<-Gra22[,-1]
+
+
+Gra55<-melt(Gra22,id.vars = "variable")
+colnames(Gra55)[1:2]<-c("crivo","Sed_ID")
+Gra66<-dcast(Gra55,Sed_ID~crivo,fun.aggregate = mean)
 
 
 ###Gradistat
@@ -65,13 +74,13 @@ G4$Sed_ID<-as.numeric(gsub("X","",G4$sedID))
 G5<-G4[order(G4$Sed_ID),]
 Gra11<-Gra1[order(Gra1$Sed_ID),]
 
-which(G5$Sed_ID!=Gra11$Sed_ID)
+#which(G5$Sed_ID!=Gra11$Sed_ID)
 
 Gra_F<-merge(Gra11,G5,all.x=T,all.y=T)
 
-#write.table(Gra_F,"Data_out/db/sediment_stats_20201109.csv",row.names=F,sep=";")
+#write.table(Gra_F,"Data_out/db/sediment_stats_20201113.csv",row.names=F,sep=";")
 
-Gra_F1<-merge(Gra6,G5,all.x=T,all.y=T)
+Gra_F1<-merge(Gra66,G5,all.x=T,all.y=T)
 
 
 
@@ -98,35 +107,37 @@ ggplot(Gra_F1,aes(x=Gra_F1$'0',y=Mean.fw.phi))+
 ################ Satellite part now
 
 ## Load GNB shape
-GNB<-readOGR("./Shapefiles/GNB/gnb_poly.shp")
-crs(GNB)
+#GNB<-readOGR("./Shapefiles/GNB/gnb_poly.shp")
+#crs(GNB)
 # Change the projection to lat long
-GNB1<-spTransform(GNB,crs("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
-crs(GNB1)
+#GNB1<-spTransform(GNB,crs("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+#crs(GNB1)
 
 
-GT<-readOGR("./Data_out/Polygons/poly_GT.shp")
+GT<-readOGR("./Data_out/Polygons/poly_GT_20201114.shp")
 
-plot(GNB1)
+#plot(GNB1)
 plot(GT, col="red")
-str(GT)
+#str(GT)
 
-GT_DF<-GT@data
-str(GT_DF)
+#GT_DF<-GT@data
+#str(GT_DF)
 
 Gra_F$Point<-as.factor(Gra_F$Sed_ID)
 
+###Check for duplicated ID
 AA<-data.frame(table(Gra_F$Point))
 AA[AA$Freq > 1,]
 
-###quick fix, deal with this repetition later
-Gra_Final<-Gra_F[-141,] ### delete duplicate entry (point 3346)
+###quick fix, deal with this sediment duplication later - check Gra_F dor the index row to delete - it changes with every new db
+Gra_Final<-Gra_F[-which(duplicated(Gra_F$Point)),] ### delete duplicate entry (point 3346)
 
 GT_Final<-merge(GT,Gra_Final,by="Point",all.x=F,all.y=T)
 
-GT_Final$Sed_class<-substr(GT_Final$Sediment,1,9)
+GT_Final$Sed_class<-substr(GT_Final$Sediment,1,11)
 
 plot(GT_Final,col="red")
+writeOGR(GT_Final,"Data_out/Polygons",layer="Poly_GT_Gra_ended_20201113",driver="ESRI Shapefile",overwrite=T)
 
 View(GT_Final@data)
 
@@ -135,6 +146,11 @@ ggplot(GT_Final@data,aes(x=Class_2,y=Sed_class))+
   geom_point(size=3.5)+
   #stat_summary(size=1)+
   scale_x_discrete(limits=c("beach_sand","sand","muddy_sand","sandy_mud","mud"))
+
+ggplot(GT_Final@data,aes(x=Class_3,y=mud,col=Class_2))+
+  #geom_point(size=3.5)+
+  stat_summary(size=1,na.rm=F)+
+  scale_x_discrete(limits=c("NA","uca","macro_uca"))
 
 ggplot(GT_Final@data,aes(x=Sed_class,y=Mean.fw.phi,col=Class_2))+
   #geom_point(size=2,position=position_jitter(width=.1, height=0))+
